b47afd87236a3c55c27932cf5854ac5201a98e24
Alex Standiford <a@alexstandiford.com>
2019-07-16 10:56:17 -0400

    Corrects the file location to match how GitHub pulls the files. #5

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,15 +8,15 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require '/tmp/wordpress/wp-content/plugins/affiliate-wp/affiliate-wp.php';
+	require '/tmp/wordpress/wp-content/plugins/AffiliateWP/affiliate-wp.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
-require '/tmp/wordpress/wp-content/plugins/affiliate-wp/tests/phpunit/affwp-testcase.php';
+require '/tmp/wordpress/wp-content/plugins/AffiliateWP/tests/phpunit/affwp-testcase.php';
 
-activate_plugin( 'affiliate-wp/affiliate-wp.php' );
+activate_plugin( 'AffiliateWP/affiliate-wp.php' );
 
 // Install AffiliateWP
 affiliate_wp_install();
